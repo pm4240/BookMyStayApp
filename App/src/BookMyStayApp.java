d5649a6bcd1097817eb68d4e5e394d7b3ce9fad9
unknown <st6983@srmist.edu.in>
2026-03-11 15:03:43 +0530

    Pranav Added uc1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+public class BookMyStayApp {
+        public static void main(String[] args) {
+
+            // Print welcome message
+            System.out.println("Welcome to the Hotel Booking System");
+
+            // Print application name and version
+            System.out.println("Application: Hotel Booking System");
+            System.out.println("Version: v1.0");
+
+            // Inform that application started successfully
+            System.out.println("Application started successfully.");
+        }
+
+}
